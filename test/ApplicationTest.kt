2009c4580226635062f60d8a14ec4badc050bbe2
Mykola Gurov <ngurov@gmail.com>
2021-02-02 22:08:04 +0100

    should limit the size of the data returned

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -3,6 +3,7 @@ package com.github.mgurov
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.github.mgurov.domain.Order
 import domain.Customer
 import io.ktor.http.*
 import kotlin.test.*
@@ -19,13 +20,13 @@ class ApplicationTest {
             }
         }
     }
+
     @Test
     fun testCustomers() {
         withTestApplication({ module(testing = true) }) {
             handleRequest(HttpMethod.Get, "/api/customers/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
-                val mapper = jacksonObjectMapper()
-                mapper.registerModule(JavaTimeModule())
+
                 val parsed: List<Customer> = mapper.readValue(response.byteContent!!)
                 assertThat(parsed.map { it.firstName to it.secondName }).containsExactlyInAnyOrder(
                     "First" to "Uno", "Second" to "Dos"
@@ -33,4 +34,18 @@ class ApplicationTest {
             }
         }
     }
+
+    @Test
+    fun `should limit the size of data returned`() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/api/orders/?limit=10").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+
+                val parsed: List<Order> = mapper.readValue(response.byteContent!!)
+                assertThat(parsed).hasSize(10)
+            }
+        }
+    }
 }
+
+private val mapper = jacksonObjectMapper().registerModule(JavaTimeModule())
